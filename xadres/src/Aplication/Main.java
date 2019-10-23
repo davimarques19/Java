02cf9353b737560f3cccfad0d1ae1a51ab6c31f1
Davi Marques <daviestudos19@gmail.com>
2019-10-23 08:43:02 -0300

    Iniciando jogo de xadres

---
+++ xadres/src/Aplication/Main.java
@@ -0,0 +1,14 @@
+package Aplication;
+
+import BoardGame.Board;
+import BoardGame.Position;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Board board = new Board(8, 7);
+
+	}
+
+}
